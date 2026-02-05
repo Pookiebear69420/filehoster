e375ef60bb0a62e74cf14f86aa29b0c655085978
MalawareGuy <pdawg0607@gmail.com>
2026-02-05 16:48:19 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'File Host',
+  description: 'Upload one file and get a link',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, background: 'black', color: 'white' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
