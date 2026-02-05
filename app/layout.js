c21f42e35ce0d2d938cfa18e5fbda1038ae715fb
MalawareGuy <pdawg0607@gmail.com>
2026-02-05 17:18:45 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,19 @@
 export const metadata = {
-  title: 'File Host',
-  description: 'Upload one file and get a link',
+  title: 'File Host - Upload & Share Files',
+  description: 'Simple file hosting service. Upload files up to 500MB and share instantly.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, background: 'black', color: 'white' }}>
+      <body style={{ 
+        margin: 0, 
+        padding: 0,
+        background: 'black', 
+        color: 'white',
+        WebkitFontSmoothing: 'antialiased',
+        MozOsxFontSmoothing: 'grayscale'
+      }}>
         {children}
       </body>
     </html>
